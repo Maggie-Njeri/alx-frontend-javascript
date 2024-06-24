d30bd236b791e058147ba873a9b7ba8fee510d69
maggie-njeri <mkiri00@gmail.com>
2024-06-24 19:57:28 +0000

    ES6 basics

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    tuttiEmployees: {
+    allEmployees: {
       ...employeesList,
     },
     getNumberOfDepartments(employeesList) {
