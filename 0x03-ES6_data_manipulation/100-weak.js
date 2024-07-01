05219b5b56821a362456e770969ab99b39563309
maggie-njeri <mkiri00@gmail.com>
2024-07-01 19:06:17 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,11 @@
-const updateUniqueItems = (map) => {
-	rt const weakMap = new WeakMap();
-	export function queryAPI(endpoint) {
-		let count = weakMap.get(endpoint) || 0;
-		count += 1;
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
 
-		if (count >= 5) {
-			throw new Error('Endpoint load is high');
-		} else {
-			weakMap.set(endpoint, count);
-		}
-	}
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  } else {
+    weakMap.set(endpoint, count);
+  }
 }
