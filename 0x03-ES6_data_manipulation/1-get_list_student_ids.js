48088b98d0613550a65691c1be1fb7b259c934e1
maggie-njeri <mkiri00@gmail.com>
2024-07-01 18:08:53 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -4,10 +4,4 @@ export default function getListStudentIds(studentList) {
 		      return studentIds;
 		    }
 	  return [];
-}export default function getListStudentIds(studentList) {
-	  if (Array.isArray(studentList)) {
-		      const studentIds = studentList.map((student) => student.id);
-		      return studentIds;
-		    }
-	  return [];
 }
